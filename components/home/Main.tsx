f05e0e6581c9402cc733cef45f19b80133ab4822
john <73786528+johnsmzr@users.noreply.github.com>
2023-12-22 22:46:14 +0100

    add button and tailwindcss setting

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -6,6 +6,14 @@
 //     return <main>主体内容: {Props.myCounter}</main>
 // }
 
-export default function Main({ myCounter }: { myCounter: number }) {
-    return <main>主体内容: {myCounter}</main>
+// export default function Main({ myCounter }: { myCounter: number }) {
+//     return <main>主体内容: {myCounter}</main>
+// }
+
+export default function Main() {
+  return (
+    <main className="flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
+      主体内容
+    </main>
+  );
 }
